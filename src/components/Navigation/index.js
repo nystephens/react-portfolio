cd622a83602bb0115e927d7c26812637d0254b3b
Dead Nate <nathan.yukio.stephens@gmail.com>
2021-04-29 10:48:30 -0700

    simple JSX return statemets made for each component.  Need to ad more HTML and CSS bootstrap

---
+++ src/components/Navigation/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Navigation() {
+    return(
+        <div>
+            <nav>
+                Navbar
+            </nav>
+        </div>
+    )
+}
+
+export default Navigation;
