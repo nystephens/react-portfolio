4de6e4db81dee47b5c589b3cfb9ee53388c650db
Dead Nate <nathan.yukio.stephens@gmail.com>
2021-06-16 08:38:03 -0700

    new resume pdf added to download link

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -7,7 +7,7 @@ function Resume() {
                 <h2>Resume</h2>
             </div>
             <div className="col-sm p-2 text-center">
-                <a href="https://docs.google.com/document/d/1VmoQcL21HcdgNjY2l7KlKnAVgzOTGFCVhHBPnAOBmjw/edit?usp=sharing" target="__blank" rel="noreferrer" className="resume-link">Download</a>
+                <a href="https://drive.google.com/file/d/14LdRt-a2W0OiMcaTwfJ6tg_jbHd54kw2/view?usp=sharing" target="__blank" rel="noreferrer" className="resume-link">Download</a>
             </div>
             <div className="col-sm p-2 text-center">
                 <h3>Proficiencies :</h3>
