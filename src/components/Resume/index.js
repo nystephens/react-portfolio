3eed907843ff88249bf9dc70122de182e865649a
Dead Nate <nathan.yukio.stephens@gmail.com>
2021-06-21 09:56:19 -0700

    professional skills added to resume section

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -7,29 +7,15 @@ function Resume() {
                 <h2>Click to view and download resume:</h2>
             </div>
             <div className="col-sm p-2 text-center">
-                    <a type="button" href="https://drive.google.com/file/d/1q0QoVbwe5yxVSs-5iG9fUQIMukwrmsd7/view?usp=sharing" target="__blank" rel="noreferrer" className="resume-link btn btn-outline-dark btn-lg">Download</a>
+                <a type="button" href="https://drive.google.com/file/d/1q0QoVbwe5yxVSs-5iG9fUQIMukwrmsd7/view?usp=sharing" target="__blank" rel="noreferrer" className="resume-link btn btn-outline-dark btn-lg">Download</a>
             </div>
             <div className="col-sm p-2 text-center">
-                <h3>Technologies :</h3>
+
                 <div className="proficiencies">
-                    <ul>
-                        <li className="list-el">HTML</li>
-                        <li className="list-el">CSS</li>
-                        <li className="list-el">Javascript</li>
-                        <li className="list-el">Git</li>
-                        <li className="list-el">Node.js</li>
-                        <li className="list-el">Express</li>
-                        <li className="list-el">REACT</li>
-                        <li className="list-el">SQL</li>
-                        <li className="list-el">mySQL</li>
-                        <li className="list-el">NoSQL</li>
-                        <li className="list-el">MongoDB</li>
-                        <li className="list-el">GraphQL</li>
-                        <li className="list-el">Handlebars</li>
-                        <li className="list-el">RESTful APIs</li>
-                        <li className="list-el">jQuery</li>
-                    </ul>
-                    {/* <p>HTML, CSS, Javascript, Git, Node.js, REACT, SQL, mySQL, NoSQL, MongoDB, GraphQL, Handlebars, RESTful APIs, jQuery</p> */}
+                    <h3>Technologies :</h3>
+                    <p>HTML, CSS, Javascript, Git, Node.js, REACT, SQL, mySQL, NoSQL, MongoDB, GraphQL, Handlebars, RESTful APIs, jQuery</p>
+                    <h3>Professional Skills:</h3>
+                    <p>Project Management, Version Control, Time Management, Organization, Troubleshooting, Customer Service, Sales, Contract Development, Engaged Learning, Supervision, Training, Teaching, Mentorship</p>
                 </div>
             </div>
         </div>
